0158ef9e140c5f5fb3810c36ef0bb61c8ebea6eb
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2017-04-13 13:11:55 +0100

    Updated licence.

--- src/main/java/uk/ac/standrews/cs/utilities/m_tree/experiments/euclidean/Point.java
+++ src/main/java/uk/ac/standrews/cs/utilities/m_tree/experiments/euclidean/Point.java
@@ -2,16 +2,16 @@
  * Copyright 2017 Systems Research Group, University of St Andrews:
  * <https://github.com/stacs-srg>
  *
- * This file is part of the module digitising-scotland-utils.
+ * This file is part of the module utilities.
  *
- * digitising-scotland-utils is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * utilities is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * digitising-scotland-utils is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * utilities is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with digitising-scotland-utils. If not, see
+ * You should have received a copy of the GNU General Public License along with utilities. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 package uk.ac.standrews.cs.utilities.m_tree.experiments.euclidean;
